de7e9649809880774f7440d28270640649fad7ca
Alumno <Alumno@LAB1504-01>
2024-03-23 12:51:58 -0500

    Agregar archivos de proyecto.

---
+++ App1/Login.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace App1
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        Menu menu = new Menu();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if(textBoxEmail.Text == "Mori" && passwordBox1.Password == "123")
+            {
+                menu.Show();
+            }
+
+            else
+            {
+                errormessage.Text = "CREDENCIALES INCORRECTAS";
+            }
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
